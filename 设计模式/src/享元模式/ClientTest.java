9a7b77424ddf568acccd23795896c61c95a3991a
Li Xingyu <xingyu.li@1step.ai>
2021-01-23 16:06:23 +0800

    项目需求

--- 设计模式/src/享元模式/ClientTest.java
+++ 设计模式/src/享元模式/ClientTest.java
@@ -7,6 +7,14 @@ package 享元模式;
 public class ClientTest {
 
     public static void main(String[] args) {
+        /**项目需求
+         * 创建游戏中的树:
+         * 特点:树的种类 是多种,同种类的树和不同种类的树的坐标相同
+         * 同种类的树:被多个地方引用 ,应该共用同一个对象
+         * 这里就引出两个概念:内部状态和外部状态
+         * 内部状态是只稳定的状态->树的种类
+         * 外部状态是不稳定的部分->树的坐标
+         * */
         final TreeFactory treeFactory = new TreeFactory();
 
         treeFactory.getTree("杨树").growTree(new Coordinate("12","34"));
