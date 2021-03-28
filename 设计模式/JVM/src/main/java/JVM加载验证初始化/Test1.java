ecea24870c884261c2f27865e2f32a8786a1a6de
Li Xingyu <xingyu.li@1step.ai>
2021-03-28 14:04:47 +0800

    edit 高并发阿斯顿撒

--- 设计模式/JVM/src/main/java/JVM加载验证初始化/Test1.java
+++ 设计模式/JVM/src/main/java/JVM加载验证初始化/Test1.java
@@ -12,6 +12,7 @@ public class Test1 {
 
     public static void main(String[] args) {
 
+        
         SimpleDateFormat formatter= new SimpleDateFormat("yyyy/MM/dd 'at' HH:mm:ss z");
         Date date = new Date(System.currentTimeMillis());
         System.out.println(formatter.format(date));
